bda7dce9fd7eac7dfe0e97ff45f646deeb5acdc8
malithj <malith.12@cse.mrt.ac.lk>
2016-06-18 20:10:59 +0530

    Log evidence calculation completed for 4 models

--- postCP/R/postcp.dev.r
+++ postCP/R/postcp.dev.r
@@ -8,7 +8,7 @@ postcp <- function(formula, data=numeric(), bp=integer(), family = gaussian, hom
   cp=bp
   CP=c(0,cp,n)
   y=rep(NA,n)
-  
+
   # compute response variable based on the family
     if(is.character(family)){
           family <- get(family, mode = "function", envir = parent.frame())
@@ -18,6 +18,9 @@ postcp <- function(formula, data=numeric(), bp=integer(), family = gaussian, hom
     }
   
     if(family$family == 'poisson'){
+      if(sum(Xbeta<0) > 0){
+        stop("Negative numbers with Poisson distribution specified, choose family=gaussian");
+      }
       # compute the response variable y
       for (k in 1:K) {
         idx=(CP[k]+1):(CP[k+1])
@@ -25,7 +28,7 @@ postcp <- function(formula, data=numeric(), bp=integer(), family = gaussian, hom
       }
       # compute the log-evidence le
       # this is basically the only model-specific part !
-      le=y*log(Xbeta)-y
+      le=y*log(Xbeta)-y-lgamma(y+1)
      }
     else if(family$family == 'gaussian'){
       # compute the response variable y
@@ -39,8 +42,36 @@ postcp <- function(formula, data=numeric(), bp=integer(), family = gaussian, hom
       le=-(Xbeta-y)^2/2/sigma^2-log(sigma)-0.5*log(2*pi)
      }
     else if(family$family == 'binomial'){
+      if(sum(Xbeta<0) > 0){
+        stop("Negative numbers with Binomial distribution specified, choose family=gaussian");
+      }
       # compute the response variable y
-     }
+      for (k in 1:K) {
+        idx=(CP[k]+1):(CP[k+1])
+        y[idx]=rbinom(diff(range(idx))+1,1,Xbeta[idx,k])
+      }
+      # compute the log-evidence le
+      # this is basically the only model-specific part !
+      le= y*log(Xbeta)+(1-y)*log(1-Xbeta)
+    }
+    else if(family$family == 'Gamma'){
+      if(sum(Xbeta<0) > 0){
+        stop("Negative numbers with Binomial distribution specified, choose family=gaussian");
+      }
+      # compute the response variable y
+      for (k in 1:K) {
+        idx=(CP[k]+1):(CP[k+1])
+        y[idx]=rgamma(diff(range(idx))+1,Xbeta[1],Xbeta[2])
+      }
+      # compute the log-evidence le
+      
+      # this is basically the only model-specific part !
+      le=(Xbeta[1]-1)*log(y)-y/Xbeta[2]-Xbeta[1]*log(Xbeta[2])-lgamma(Xbeta[1])
+    }
+    else{
+      print(family)
+      stop("'family' not recognized")
+    }
   
   # build workspace for forward-backward
   lFw=matrix(0,n,K)
@@ -57,7 +88,7 @@ postcp <- function(formula, data=numeric(), bp=integer(), family = gaussian, hom
     aux=lFw[-n,k]+le[-1,k+1]+lBk[-1,k+1]
     post.cp[-n,k]=exp(aux-max(aux))
   }
-  return(list(loglik=loglik,post=post,post.cp=post.cp))
+  return(list(n=n,loglik=loglik,post=post,post.cp=post.cp))
 }
 
 logsumexp=function(l) {
